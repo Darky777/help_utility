93bd05c7153b247a73b0c73fd5b03f943acc0073
Ivan <stn_iv@list.ru>
2019-02-04 12:17:21 +0300

    Added the qsim funtion that must be includes in .bashrc file

---
+++ qsim_config.bash
@@ -0,0 +1,15 @@
+#must be in .bashrc file
+path_to_freetype=/home/ivan/lib_for_questa/freetype-2.4.7-64bit/lib
+qsim_config_f () {
+    local path=$(export | grep "$path_to_freetype")
+    if [[ -z ${path} ]]; then
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$path_to_freetype
+        echo "freetype-2.4.7 for Questasim added"
+        echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+    else
+        local removing_string=${LD_LIBRARY_PATH}
+        export LD_LIBRARY_PATH=${LD_LIBRARY_PATH#"$removing_string"}
+        echo "freetype-2.4.7 for Questasim removed"
+        echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+    fi
+}
